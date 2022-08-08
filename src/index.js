7aed01660600833ba7368bd8146637df575bdaef
Valeriia <volikavs@gmail.com>
2022-08-08 10:51:26 +0200

    all works

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App.js';
+import { App } from 'components/App';
 import './index.css';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './components/theme.js';
+import { theme } from 'components/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
